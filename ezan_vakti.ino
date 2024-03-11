7469bb74624996c143b80fa707d1cc76b1c240ca
HamzaYslmn <hamzayesilmen7@gmail.com>
2024-03-11 20:24:01 +0300

    go

--- ezan_vakti.ino
+++ ezan_vakti.ino
@@ -17,18 +17,36 @@ String calculationMethod = "Turkey";
 String latitude = "39.91987";
 String longitude = "32.85427";
 
+int sec60 = 60;
+
 String imsak;
 String gunes;
 String ogle;
 String ikindi;
 String aksam;
 String yatsi;
+String current_time_short;
+String aksam_short;
+String imsak_short;
+String tarih_short;
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", time_offset_saniye);
 
+// Pin tanımlamaları
+const int D1_PIN = D1;
+const int D2_PIN = D2;
+const int ONBOARD_LED = 2; // D4 pinine karşılık gelen GPIO pin numarası
+
 void setup() {
   Serial.begin(115200);
+  pinMode(D1_PIN, OUTPUT);
+  pinMode(D2_PIN, OUTPUT);
+  pinMode(ONBOARD_LED, OUTPUT);
+  digitalWrite(D1_PIN, LOW);
+  digitalWrite(D2_PIN, LOW);
+  digitalWrite(ONBOARD_LED, LOW);
+
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -38,9 +56,30 @@ void setup() {
 
   Serial.println("WiFi bağlantısı başarılı");
   timeClient.begin();
+  getFormattedDate();
+
+  while (tarih_short == "1970" || tarih_short == "") {
+    getFormattedDate();
+    tarih_short = current_date.substring(0, 4);
+    delay(1000);
+  }
+
+  getPrayerTimes();
+  
+  while (imsak_short == "null" || imsak_short == "") {
+    getPrayerTimes();
+    imsak_short = imsak.substring(0, 4);
+    delay(1000);
+  }
+
+  digitalWrite(ONBOARD_LED, HIGH); // Onboard LED'i söndür (ters çalışır)
+  Serial.println("Hazır!");
+  Serial.println("------------------------------------------");
+  
 }
 
 void getFormattedDate() {
+  timeClient.update();
   long epochTime = timeClient.getEpochTime();
   struct tm *timeinfo;
   time_t rawtime = epochTime;
@@ -48,36 +87,42 @@ void getFormattedDate() {
   char formattedDate[11];
   sprintf(formattedDate, "%04d-%02d-%02d", timeinfo->tm_year + 1900, timeinfo->tm_mon + 1, timeinfo->tm_mday);
   current_date = String(formattedDate);
-  Serial.println("Tarih: " + current_date);
+  Serial.print("Tarih: ");
+  Serial.println(current_date);
   current_time = timeClient.getFormattedTime();
-  Serial.println("Saat: " + current_time);
+  Serial.print("Saat: ");
+  Serial.println(current_time);
 }
 
 
 void getPrayerTimes() {
-  if ((WiFi.status() == WL_CONNECTED)) {
-
-    std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
-    // SSL sertifikası doğrulamasını yok say
+  if (WiFi.status() == WL_CONNECTED) {
+    std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
     client->setInsecure();
     
-    // HTTPClient örneği oluştur
     HTTPClient https;
 
-    // Namaz vakitlerini almak için API isteği yap
     String url = "https://namaz-vakti.vercel.app/api/timesFromCoordinates?lat=" + latitude + "&lng=" + longitude + "&date=" + current_date + "&days=1&timezoneOffset=" + time_offset_dakika + "&calculationMethod=" + calculationMethod;
-    https.begin(*client, url); // HTTP isteğini başlat
+    https.begin(*client, url);
+    Serial.println("API isteği gönderiliyor: " + url);
 
-    int httpCode = https.GET(); // GET isteği gönder
+    int httpCode = https.GET();
 
-    if (httpCode > 0) { // Başarılı bir HTTP yanıtı alındıysa
-      if (httpCode == HTTP_CODE_OK) { // 200 OK yanıtı alındıysa
-        String payload = https.getString(); // Yanıtı al
-        Serial.println("Namaz vakitleri: " + payload); // Seri monitöre yazdır
+    if (httpCode > 0) {
+      if (httpCode == HTTP_CODE_OK) {
+        String payload = https.getString();
 
-        // JSON verilerini ayrıştır
+        Serial.println("Received payload from API:");
+        Serial.println(payload);
+
         DynamicJsonDocument doc(1024);
-        deserializeJson(doc, payload);
+        DeserializationError error = deserializeJson(doc, payload);
+
+        if (error) {
+          Serial.print("deserializeJson() failed: ");
+          Serial.println(error.c_str());
+          return;
+        }
 
         JsonObject place = doc["place"];
         JsonObject times = doc["times"];
@@ -86,7 +131,7 @@ void getPrayerTimes() {
         float lat = place["latitude"];
         float lon = place["longitude"];
 
-        // Belirtilen tarihin namaz vakitlerini al
+        Serial.println(current_date);
         JsonArray prayerTimes = times[current_date.c_str()];
 
         Serial.println("Namaz Saatleri: " + city);
@@ -113,17 +158,30 @@ void getPrayerTimes() {
       Serial.println("HTTP isteği başarısız");
     }
 
-    https.end(); // HTTP isteğini sonlandır
-  }
-  else {
+    https.end();
+  } else {
     Serial.println("WiFi bağlantısı yok");
   }
 }
 
-
 void loop() {
-  timeClient.update();
   getFormattedDate();
-  getPrayerTimes();
   delay(1000);
+
+  current_time_short = current_time.substring(0, 5);
+  aksam_short = aksam.substring(0, 5);
+  imsak_short = imsak.substring(0, 5);
+  if (current_time_short == imsak_short) {
+    digitalWrite(D1_PIN, HIGH); // D1 pini açık
+    Serial.println("Sahur vakti bitti, niyetlenmeyi unutmayın!");
+  } else {
+    digitalWrite(D1_PIN, LOW); // D1 pini kapalı
+  }
+
+  if (current_time_short == aksam_short) {
+    digitalWrite(D2_PIN, HIGH); // D2 pini açık
+    Serial.println("İftar Saati: Allah kabul etsin");
+  } else {
+    digitalWrite(D2_PIN, LOW); // D2 pini kapalı
+  }  
 }
